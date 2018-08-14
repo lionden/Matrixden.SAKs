50b303ef3bb368f03df43e1dd66dc3a705f01b93
The Architect <liondenlee@hotmail.com>
2018-08-14 11:04:07 +0800

    *: Modify assembly info.

--- Src/Matrixden/GlobalAssemblyInfo.cs
+++ Src/Matrixden/GlobalAssemblyInfo.cs
@@ -3,9 +3,9 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("山东云媒软件股份有限公司")]
-[assembly: AssemblyProduct("讯猫智能便利店管理系统")]
-[assembly: AssemblyCopyright("Copyright © Clouds 2017")]
+[assembly: AssemblyCompany("Lionden.Org")]
+[assembly: AssemblyProduct("Matrixden")]
+[assembly: AssemblyCopyright("Copyright © Lionden 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,5 +19,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
-[assembly: AssemblyVersion("2.4.9629.0")]
-[assembly: AssemblyFileVersion("2.4.9629.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
