45ce9e74a301777babd613be6b24a42c2d6da1d2
LiondenLee <hsdlionden@hotmail.com>
2018-11-12 14:31:54 +0800

    *: Rebuild the code.

--- Src/Matrixden/Matrixden.Utils.Serialization/JsonHelper.cs
+++ Src/Matrixden/Matrixden.Utils.Serialization/JsonHelper.cs
@@ -10,69 +10,163 @@
     using System.Runtime.Serialization.Json;
     using System.Text;
 
+    /// <summary>
+    /// 
+    /// </summary>
     public static class JsonHelper
     {
         private static readonly ILog log = LogProvider.GetCurrentClassLogger();
         private static readonly IsoDateTimeConverter datetimeConverter = new IsoDateTimeConverter();
         public static readonly Encoding encoding = Encoding.UTF8;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public static byte[] SerializeToJsonByte(object obj)
-        {
-            string json = SerializeToJsonString(obj);
-            byte[] buffer = encoding.GetBytes(json);
-
-            return buffer;
-        }
+        #region -- Serialize --
 
         /// <summary>
-        /// 
+        /// Serialize object to JSON.
         /// </summary>
         /// <param name="obj"></param>
         /// <param name="ignoreNullable"></param>
         /// <returns></returns>
-        public static string SerializeToJsonString(object obj, bool ignoreNullable = false)
+        public static string Serialize(object obj, bool ignoreNullable)
         {
-            string json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
-                                        {
-                                            DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffffffZ",
-                                            NullValueHandling = ignoreNullable ? NullValueHandling.Ignore : NullValueHandling.Include
-                                        });
-            ////Workaround, for string, int type value.
-            if (!json.IsJson())
-                return obj.ToString();
-
-            return json;
-        }
-
-        public static byte[] ToByteArray(string input)
-        {
-            if (input.IsNullOrEmptyOrWhiteSpace())
+            var json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
             {
-                return null;
-            }
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffffffZ",
+                NullValueHandling = ignoreNullable ? NullValueHandling.Ignore : NullValueHandling.Include
+            });
 
-            return encoding.GetBytes(input);
+            ////Workaround, for string, int type value.
+            return !json.IsJson() ? obj.ToString() : json;
         }
 
         /// <summary>
-        /// 
+        /// Serialize object to JSON.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string Serialize(object obj) => Serialize(obj, false);
+
+        /// <summary>
+        /// Serialize object to bytes.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static byte[] Serialize2Bytes(object obj)
+        {
+            var json = Serialize(obj);
+            var buffer = json.Bytes(encoding);
+
+            return buffer;
+        }
+
+        #endregion
+
+        #region -- Deserialize --
+
+        /// <summary>
+        /// 反序列化JSON字符串到Object
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static object Deserialize(string input)
+        {
+            if (input.IsNullOrEmptyOrWhiteSpace())
+                return default(object);
+
+            var obj = default(object);
+            try
+            {
+                obj = JsonConvert.DeserializeObject(input);
+            }
+            catch (Exception ex)
+            {
+                log.ErrorException("Failed to deserialize from string value[{0}].", ex, input);
+            }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Deserialize a string value to an entity, using Newtonsoft.Json.JsonConvert.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">The string value to be Deserialized.</param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string input)
+        {
+            if (input.IsNullOrEmptyOrWhiteSpace())
+                return default(T);
+
+            if (!input.IsJson())
+                return input.ToT<T>();
+
+            var entity = default(T);
+            try
+            {
+                entity = JsonConvert.DeserializeObject<T>(input);
+            }
+            catch (Exception e)
+            {
+                log.ErrorException("Failed to parse JSON string to type [{0}], Origin String=[{1}].", e,
+                    typeof(T).AssemblyQualifiedName, input);
+            }
+
+            return entity;
+        }
+
+        /// <summary>
+        /// Deserialize a byte array to an entity, using Newtonsoft.Json.JsonConvert.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(byte[] bytes) => Deserialize<T>(encoding.GetString(bytes));
+
+        /// <summary>
+        /// 反序列化JSON字符串到JObject
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static JObject Deserialize2JObject(string input) => (JObject)Deserialize(input);
+
+        /// <summary>
+        /// Deserialize a string value to an entity, using System.Runtime.Serialization.Json.DataContractJsonSerializer.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input">The string value to be Deserialized.</param>
+        /// <returns></returns>
+        public static T Deserialize2<T>(string input)
+        {
+            if (input.IsNullOrEmptyOrWhiteSpace())
+                return default(T);
+
+            if (!input.IsJson())
+                return input.ToT<T>();
+
+            var obj = default(object);
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            using (var mStream = new MemoryStream(encoding.GetBytes(input)))
+            {
+                obj = serializer.ReadObject(mStream);
+            }
+
+            return (T)obj;
+        }
+
+        /// <summary>
+        /// Deserialize stream to T.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="stream"></param>
         /// <returns></returns>
-        public static T GetRequestEntity<T>(Stream stream)
+        public static T Deserialize<T>(Stream stream)
         {
-            string data = string.Empty;
+            var data = string.Empty;
             using (var ms = new MemoryStream())
             {
-                int d = -1;
                 try
                 {
+                    var d = -1;
                     while ((d = stream.ReadByte()) != -1)
                     {
                         ms.WriteByte((byte)d);
@@ -86,100 +180,9 @@
                 }
             }
 
-            return DeserializeStringToObject<T>(data);
+            return Deserialize<T>(data);
         }
 
-        /// <summary>
-        /// 将JSON格式字符串转实体
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static T GetEntityFromString<T>(string input)
-        {
-            if (!input.IsJson())
-                return input.ToT<T>();
-
-            return DeserializeStringToObject<T>(input);
-        }
-
-        /// <summary>
-        /// 反序列化JSON字符串到JObject
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static JObject DeserializeStringToJObject(string input)
-        {
-            return (JObject)DeserializeStringToObject(input);
-        }
-
-        /// <summary>
-        /// 反序列化JSON字符串到Object
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static object DeserializeStringToObject(string input)
-        {
-            if (input.IsNullOrEmptyOrWhiteSpace())
-                return null;
-
-            try
-            {
-                return JsonConvert.DeserializeObject(input);
-            }
-            catch (Exception ex)
-            {
-                log.ErrorException("Failed to deserialize from string value[{0}].", ex, input);
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Deserialize a string value to an entity, using Newtonsoft.Json.JsonConvert.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="input">The string value to be Deserialized.</param>
-        /// <returns></returns>
-        public static T DeserializeStringToObject<T>(string input)
-        {
-            if (input.IsNullOrEmptyOrWhiteSpace())
-                return default(T);
-
-            if (!input.IsJson())
-                return input.ToT<T>();
-
-            T entity = default(T);
-            try
-            {
-                entity = JsonConvert.DeserializeObject<T>(input);
-            }
-            catch (Exception e)
-            {
-                log.ErrorException("Failed to parse JSON string to type [{0}], Origin String=[{1}].", e, typeof(T).AssemblyQualifiedName, input);
-            }
-
-            return entity;
-        }
-
-        /// <summary>
-        /// Deserialize a string value to an entity, using System.Runtime.Serialization.Json.DataContractJsonSerializer.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="input">The string value to be Deserialized.</param>
-        /// <returns></returns>
-        public static T DeserializeStringToObject2<T>(string input)
-        {
-            if (input.IsNullOrEmptyOrWhiteSpace())
-                return default(T);
-
-            object obj = null;
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-            using (var mStream = new MemoryStream(Encoding.UTF8.GetBytes(input)))
-            {
-                obj = serializer.ReadObject(mStream);
-            }
-
-            return (T)obj;
-        }
+        #endregion
     }
 }
