0707d4a12319e72b5f215960aaca7c38dc1f9588
Lionden W. Lee <liondenlee@hotmail.com>
2020-07-24 13:49:41 +0800

    +: Add audio util project.

---
+++ Src/Matrixden/Matrixden.Utils.Audio/AudioUtil.cs
@@ -0,0 +1,22 @@
+﻿using Matrixden.Utils.Audio.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrixden.Utils.Audio
+{
+    public class AudioUtil
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int freq, int duration);
+
+        public static void Beep() => Beep(800, 300);
+    }
+}
---
+++ Src/Matrixden/Matrixden.Utils.Audio/TextSpeaker.cs
@@ -0,0 +1,70 @@
+﻿using Matrixden.Utils.Audio.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrixden.Utils.Audio
+{
+    public class TextSpeaker : AudioUtil
+    {
+        static readonly ILog _LOG = LogProvider.GetCurrentClassLogger();
+        string _pre_text = string.Empty;
+        Prompt _prompt = new Prompt(string.Empty);
+        public void SpeakAsync(string txt)
+        {
+            if (!_prompt.IsCompleted)
+            {
+                if (txt.Equals(_pre_text))
+                    return;
+                else
+                    Synth.SpeakAsyncCancel(_prompt);
+            }
+
+            _pre_text = txt;
+            _prompt = Synth.SpeakAsync(txt);
+        }
+
+        internal void SpeakAsyncCancelAll() => Synth.SpeakAsyncCancelAll();
+
+        // Initialize a new instance of the SpeechSynthesizer.  
+        SpeechSynthesizer Synth { get; } = new SpeechSynthesizer();
+
+        private static object __locker = new object();
+        private static TextSpeaker __instance;
+        public static TextSpeaker Instance
+        {
+            get
+            {
+                if (__instance == null)
+                {
+                    lock (__locker)
+                    {
+                        if (__instance == null)
+                        {
+                            __instance = new TextSpeaker();
+                        }
+                    }
+                }
+
+                return __instance;
+            }
+        }
+
+        private TextSpeaker()
+        {
+            // Configure the audio output.   
+            Synth.SetOutputToDefaultAudioDevice();
+            Synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.NotSet, 0, CultureInfo.GetCultureInfo("zh-CN"));
+
+            if (Synth.Voice.Culture.Name != "zh-CN")
+            {
+                Beep();
+                _LOG.Fatal("该机器未安装中文语音");
+            }
+        }
+    }
+}
