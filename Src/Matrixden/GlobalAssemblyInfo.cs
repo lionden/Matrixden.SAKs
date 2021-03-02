c234d85846c2c9e7aaebf2cb449bbc32cfca6475
Lionden W. Lee <liondenlee@hotmail.com>
2021-03-02 14:55:59 +0800

    *: Update Matrixden.Utils to SwissArmyKnives, short is SAK. And not support for legacy version than 4.6 framework.

--- Src/Matrixden/GlobalAssemblyInfo.cs
+++ Src/Matrixden/GlobalAssemblyInfo.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("2.0.2101.13.1@LPC")]
-[assembly: AssemblyVersion("2.0.2101.13")]
+[assembly: AssemblyInformationalVersion("3.0.2103.02.1@LPC")]
+[assembly: AssemblyVersion("3.0.2103.02")]
