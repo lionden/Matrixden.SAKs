eaa167765179a547b7cb0e6729bb593e699c9f80
Lionden W. Lee <liondenlee@hotmail.com>
2021-03-03 15:27:16 +0800

    +: Add util class for process process.

---
+++ Src/Matrixden/Matrixden.Test/SAKs/TestProcessUtil.cs
@@ -0,0 +1,46 @@
+﻿using Matrixden.SwissArmyKnives.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace Matrixden.Test.SAKs
+{
+    [TestClass]
+    public class TestProcessUtil
+    {
+        [TestMethod]
+        public void TestGets()
+        {
+            var ps = ProcessUtil.Gets("Authentication.Tray");
+            Assert.IsTrue(ps.Length >= 1);
+        }
+
+        [TestMethod]
+        public void TestEnd()
+        {
+            ProcessUtil.EndTask("Authentication.Tray");
+
+            Thread.Sleep(200);
+            var ps = ProcessUtil.Gets("Authentication.Tray");
+            Assert.IsTrue(ps.Length <= 0);
+        }
+
+        [TestMethod]
+        public void TestStart()
+        {
+            var b = ProcessUtil.Start(@"C:\Users\Lionden Lee\source\repos\BioidentificationCenter\_Build\release\JBSS Client\Authentication.Tray.exe");
+            Assert.IsTrue(b);
+
+            TestGets();
+        }
+
+        [TestMethod]
+        public void TestRestart()
+        {
+            var b = ProcessUtil.ReStart(@"C:\Users\Lionden Lee\source\repos\BioidentificationCenter\_Build\release\JBSS Client\Authentication.Tray.exe");
+            Assert.IsTrue(b);
+
+            TestGets();
+        }
+    }
+}
