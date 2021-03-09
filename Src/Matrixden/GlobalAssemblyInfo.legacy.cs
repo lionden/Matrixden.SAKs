226703af766078c02477a1f6ccbcac0f8b53698e
Lionden W. Lee <liondenlee@hotmail.com>
2021-03-09 14:08:52 +0800

    +: Splide assembly info file.

--- Src/Matrixden/GlobalAssemblyInfo.legacy.cs
+++ Src/Matrixden/GlobalAssemblyInfo.legacy.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("3.0.2103.09.1@LPC")]
-[assembly: AssemblyVersion("3.0.2103.09")]
+[assembly: AssemblyInformationalVersion("2.1.2103.09.1@LPC")]
+[assembly: AssemblyVersion("2.1.2103.09")]
