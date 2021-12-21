8d58b0dc15ea6aa7f140d877b06380f37bc06d47
Lionden W. Lee <liondenlee@hotmail.com>
2021-12-21 14:03:27 +0800

    *: Update version to 12.21.

--- Src/Matrixden/GlobalAssemblyInfo.cs
+++ Src/Matrixden/GlobalAssemblyInfo.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("3.0.2112.09.1@LPC")]
-[assembly: AssemblyVersion("3.0.2112.09")]
+[assembly: AssemblyInformationalVersion("3.0.2112.12.1@LPC")]
+[assembly: AssemblyVersion("3.0.2112.21")]
