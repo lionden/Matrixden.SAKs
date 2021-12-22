69331cd65dd6e80aafa7c0d848ea83fddd636aa3
Lionden W. Lee <liondenlee@hotmail.com>
2021-12-22 14:36:59 +0800

    +: Add unit test project for legacy matrixden project.

---
+++ Src/Matrixden/Matrixden.Utils.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Matrixden.Utils.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Matrixden.Utils.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7944a321-d0d2-4e54-9aab-c2833d2a4903")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Src/Matrixden/Matrixden.Utils.Test/TestExtension.cs
@@ -0,0 +1,16 @@
+﻿using Matrixden.Utils.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Matrixden.Utils.Test
+{
+    [TestClass]
+    public class TestExtension
+    {
+        [TestMethod]
+        public void TestObject2Boolean()
+        {
+            Assert.IsFalse(default(object).ToBoolean(), "Should be false.");
+        }
+    }
+}
