a8cf09209a8923f62e6c4873032c4a3954bf6f49
Lionden Lee <hsdlionden@hotmail.com>
2023-04-23 03:39:56 +0800

    +: 添加坐标、轴对称点等方法。

---
+++ Src/Matrixden/Matrixden.SwissArmyKnives/Windows/MLine.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrixden.SwissArmyKnives.Windows
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MLine
+    {
+        /// <summary>
+        /// Start point
+        /// </summary>
+        public MPoint StartPoint { get; set; }
+        /// <summary>
+        /// End point
+        /// </summary>
+        public MPoint EndPoint { get; set; }
+
+        /// <summary>
+        /// Length
+        /// </summary>
+        public double Length
+        {
+            get
+            {
+                return EndPoint - StartPoint;
+            }
+        }
+
+        private Directions _direct;
+
+        /// <summary>
+        /// Direction
+        /// </summary>
+        public Directions Direction
+        {
+            get
+            {
+                if (_direct == default)
+                {
+                    if (StartPoint.X == EndPoint.X)
+                    {
+                        _direct = Directions.YParalle;
+                    }
+                    else if (StartPoint.Y == EndPoint.Y)
+                        _direct = Directions.XParalle;
+                    else
+                        _direct = Directions.Tilt_axial;
+                }
+
+                return _direct;
+            }
+            private set
+            {
+                _direct = value;
+            }
+        }
+
+        /// <summary>
+        /// [not support now]
+        /// 
+        /// from x-axis to y-axis.
+        /// Define the top-left as start point.
+        /// </summary>
+        [Obsolete("Not implemented yet", true)]
+        public double Angle { get; set; }
+
+        /// <summary>
+        /// Init a MLine object with 2 points.
+        /// </summary>
+        /// <param name="startPoint"></param>
+        /// <param name="endPoint"></param>
+        public MLine(MPoint startPoint, MPoint endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        /// <summary>
+        /// Init a MLine object with 2 points.
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        public MLine(double x1, double y1, double x2, double y2) : this(new MPoint(x1, y1), new MPoint(x2, y2)) { }
+
+        /// <summary>
+        /// Init a MLine object with a start point, direction and length.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="direction"></param>
+        /// <param name="length"></param>
+        public MLine(MPoint point, Directions direction, double length)
+        {
+            if (point == null)
+                point = new MPoint(0);
+
+            Direction = direction;
+            switch (direction)
+            {
+                case Directions.XAxis:
+                    StartPoint = point;
+                    EndPoint = point.Off(length, 0);
+                    break;
+                case Directions.YAxis:
+                    StartPoint = point;
+                    EndPoint = point.Off(0, length);
+                    break;
+                case Directions.XAxis_Anti:
+                    StartPoint = point;
+                    EndPoint = point.Off(length * -1, 0);
+                    break;
+                case Directions.YAxis_Anti:
+                    StartPoint = point;
+                    EndPoint = point.Off(0, length * -1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Represents a line paralle to the X-axis
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static MLine LineParallelXAxis(double y) => new MLine(double.MinValue, y, double.MaxValue, y);
+
+        /// <summary>
+        /// Represents a line paralle to the Y-axis
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static MLine LineParallelYAxis(double x) => new MLine(x, double.MinValue, x, double.MaxValue);
+    }
+
+    /// <summary>
+    /// Direction enums
+    /// </summary>
+    [Flags]
+    public enum Directions
+    {
+        /// <summary>
+        /// Parallel to the direction of the +x-axis
+        /// </summary>
+        XAxis = 1,
+        /// <summary>
+        /// Parallel to the direction of the +y-axis
+        /// </summary>
+        YAxis = 1 << 1,
+        /// <summary>
+        /// Antiparallel to the direction of the +x-axis
+        /// </summary>
+        XAxis_Anti = 1 << 2,
+        /// <summary>
+        /// Antiparallel to the direction of the +y-axis
+        /// </summary>
+        YAxis_Anti = 1 << 3,
+        /// <summary>
+        /// Parallel to x-axis
+        /// </summary>
+        XParalle = XAxis | XAxis_Anti,
+        /// <summary>
+        /// Parallel to y-axis
+        /// </summary>
+        YParalle = YAxis | YAxis_Anti,
+        /// <summary>
+        /// a tilt line
+        /// </summary>
+        [Obsolete("Not implemented yet")]
+        Tilt_axial = 0xffff
+    }
+}
---
+++ Src/Matrixden/Matrixden.SwissArmyKnives/Windows/MPoint.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+
+namespace Matrixden.SwissArmyKnives.Windows
+{
+    /// <summary>
+    /// Represents an Matrixden 2D point object.
+    /// Named as BMW's MPower😀.
+    /// </summary>
+    public class MPoint
+    {
+        /// <summary>
+        /// X-axis value
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y-axis value
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Init a MPoint object.
+        /// </summary>
+        /// <param name="x">X-axis value</param>
+        /// <param name="y">Y-axis value</param>
+        public MPoint(double x, double y)
+        {
+            this.X = x; this.Y = y;
+        }
+
+        /// <summary>
+        /// Init a MPoint object with a single value when the x and y-axis are equal.
+        /// </summary>
+        /// <param name="coord">X or y-axis value</param>
+        public MPoint(double coord) : this(coord, coord) { }
+
+        /// <summary>
+        /// Init a MPoint object with a <c>System.Draw.Point</c> object.
+        /// </summary>
+        /// <param name="point"></param>
+        public MPoint(Point point)
+        {
+            if (point == null)
+                X = Y = 0;
+            else
+                X = point.X; Y = point.Y;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Judge whether the 2 points are equal.
+        /// </summary>
+        /// <param name="obj">The target object</param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return this == null;
+
+            if (this == null) return false;
+
+            if (!(obj is MPoint))
+                return false;
+
+            var pnt = (MPoint)obj;
+            return this.X == pnt.X && this.Y == pnt.Y;
+        }
+
+        /// <summary>
+        /// Judge whether the 2 points are equal.
+        /// </summary>
+        /// <param name="left">The 1st point</param>
+        /// <param name="right">The 2nd point</param>
+        /// <returns></returns>
+        public static bool operator ==(MPoint left, MPoint right) => left.Equals(right);
+
+        /// <summary>
+        /// Judge whether the 2 points are not equal.
+        /// </summary>
+        /// <param name="left">The 1st point</param>
+        /// <param name="right">The 2nd point</param>
+        /// <returns></returns>
+        public static bool operator !=(MPoint left, MPoint right) => !left.Equals(right);
+
+        /// <summary>
+        /// The distance between 2 points.
+        /// </summary>
+        /// <param name="left">The 1st point</param>
+        /// <param name="right">The 2nd point</param>
+        /// <returns>The distance</returns>
+        public static double operator -(MPoint left, MPoint right)
+        {
+            if (left == null)
+            {
+                left = new MPoint(0);
+            }
+
+            if (right == null)
+            {
+                right = new MPoint(0);
+            }
+
+            return Math.Sqrt((Math.Pow(right.X - left.X, 2) + Math.Pow(right.Y - left.Y, 2)));
+        }
+    }
+}
