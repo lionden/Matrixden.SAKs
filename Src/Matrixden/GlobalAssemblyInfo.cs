bfdbbdb0d2daf1220751f0d6d983a1b41ca3ec51
Lionden W. Lee <liondenlee@hotmail.com>
2023-04-23 12:58:05 +0800

    *: Update version to 3.2.2304.23.

--- Src/Matrixden/GlobalAssemblyInfo.cs
+++ Src/Matrixden/GlobalAssemblyInfo.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("3.2.2304.23.1@LPC")]
-[assembly: AssemblyVersion("3.2.2304.23")]
+[assembly: AssemblyInformationalVersion("3.2.2304.23.2@LPC")]
+[assembly: AssemblyVersion("3.2.2304.232")]
