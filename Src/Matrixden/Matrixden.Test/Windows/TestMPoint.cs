35e1aab2148027bc5f61bfefa36e019f33d68f93
Lionden Lee <hsdlionden@hotmail.com>
2023-05-04 23:44:08 +0800

    +: 添加单元测试方法。

---
+++ Src/Matrixden/Matrixden.Test/Windows/TestMPoint.cs
@@ -0,0 +1,24 @@
+﻿using Matrixden.SwissArmyKnives.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+
+namespace Matrixden.Test.Windows
+{
+    [TestClass]
+    public class TestMPoint
+    {
+        [TestMethod]
+        public void TestAxisymmetricPoint()
+        {
+            var mp = new MPoint(955, 443);
+            var tmir = new MPoint(955, 545);
+
+            var mrp = mp.AxisymmetricPoint(new Rectangle(327, 283, 1008, 422), Directions.XParalle);
+            Assert.AreEqual(tmir, mrp);
+
+            var mrp2 = tmir.AxisymmetricPoint(new Rectangle(327, 283, 1008, 422), Directions.XParalle);
+            Assert.AreEqual(mp, mrp2);
+        }
+    }
+}
