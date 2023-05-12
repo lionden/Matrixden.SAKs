2fcb63bd639cd68e457ac8bfae2c00294d4714fc
Lionden W. Lee <liondenlee@hotmail.com>
2023-05-12 14:36:32 +0800

    +: 添加类注释; *: 升级版本号到0512.

--- Src/Matrixden/GlobalAssemblyInfo.cs
+++ Src/Matrixden/GlobalAssemblyInfo.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("3.2.2304.23.5@LPC")]
-[assembly: AssemblyVersion("3.2.2304.235")]
+[assembly: AssemblyInformationalVersion("3.2.2305.12.1@LPC")]
+[assembly: AssemblyVersion("3.2.2305.121")]
