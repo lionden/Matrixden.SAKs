d889de7847cfb721616a3f99332a994022523273
Lionden Lee <hsdlionden@hotmail.com>
2023-12-25 04:16:17 +0800

    #

--- Src/GlobalAssemblyInfo.cs
+++ Src/GlobalAssemblyInfo.cs
@@ -20,5 +20,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")
 //↓↓↓ Product version
-[assembly: AssemblyInformationalVersion("3.3.2310.24.2@LPC")]
-[assembly: AssemblyVersion("3.3.2310.242")]
+[assembly: AssemblyInformationalVersion("3.3.2312.21.1@LPC")]
+[assembly: AssemblyVersion("3.3.2312.211")]
