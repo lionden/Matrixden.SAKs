2f0d1a9db6fd3e2eb4323bc1ad3734166c9274b9
Lionden Lee <hsdlionden@hotmail.com>
2024-03-10 15:41:53 +0800

    +: Add new project of WinForm controls.

---
+++ Src/Matrixden.SAK.Controls.WinForm/MProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Matrixden.SAK.Controls.WinForm
+{
+    public class MProgressBar : ProgressBar
+    {
+        public MProgressBar()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rect = ClientRectangle;
+            Graphics g = e.Graphics;
+
+            ProgressBarRenderer.DrawHorizontalBar(g, rect);
+            rect.Width = (int)(rect.Width * ((double)Value / Maximum)) - 4;
+            rect.Height -= 4;
+            g.FillRectangle(new SolidBrush(ForeColor), 2, 2, rect.Width, rect.Height);
+
+            // 绘制进度值
+            string progressText = $"{Value}%";
+            SizeF textSize = g.MeasureString(progressText, Font);
+            PointF textLocation = new PointF((Width - textSize.Width) / 2, (Height - textSize.Height) / 2);
+            g.DrawString(progressText, Font, Brushes.Black, textLocation);
+        }
+    }
+}
---
+++ Src/Matrixden.SAK.Controls.WinForm/MVerticalProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Matrixden.SAK.Controls.WinForm
+{
+    /// <summary>
+    /// Vertical progress bar.
+    /// </summary>
+    public class MVerticalProgressBar : ProgressBar
+    {
+        public MVerticalProgressBar()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Debug.WriteLine($"{Value}");
+            Rectangle rect = ClientRectangle;
+            Graphics g = e.Graphics;
+
+            ProgressBarRenderer.DrawVerticalBar(g, rect);
+            rect.Height = (int)(rect.Height * ((double)Value / Maximum)) - 4;
+            rect.Width -= 4;
+            g.FillRectangle(new SolidBrush(ForeColor), 2, ClientRectangle.Bottom - rect.Height - 2, rect.Width, rect.Height);
+
+            // 绘制进度值
+            string progressText = $"{Value}";
+            SizeF textSize = g.MeasureString(progressText, Font);
+            PointF textLocation = new(Width / 2 - textSize.Width / 2, (Height - textSize.Height) / 2);
+            g.DrawString(progressText, Font, Brushes.Black, textLocation);
+        }
+    }
+}
